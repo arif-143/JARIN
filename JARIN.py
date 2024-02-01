df5ce70a805595c61a394809392f0470363610ab
arif-143 <158057368+arif-143@users.noreply.github.com>
2024-02-01 23:51:10 +0600

    Add files via upload

--- JARIN.py
+++ JARIN.py
@@ -268,7 +268,7 @@ def api2(ids,names,passlist):
             fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
             fbbv = str(random.randint(000000000,999999999))
             accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
-            uaddx = f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/89.0.0.1450;FBBV/2345574;FBDM/{density=',width=720,height=1280};FBLC/en_US;FBRV/1632666;FBCR/null;FBMF/Huawei;FBBD/Huawei;FBPN/com.facebook.lite;FBDV/P30 Pro;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
+            uaddx = f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[[FBAN/FB4A;FBAV/38.0.0.3137;FBBV/8730088;FBDM/{density=0,width=720,height=1280};FBLC/en_US;FBRV/5477079;FBCR/null;FBMF/Huawei;FBBD/Huawei;FBPN/com.facebook.lite;FBDV/P40 Lite;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
             head = {'User-Agent':uaddx,
             'Accept-Encoding':'gzip, deflate',
             'Connection':'close',
@@ -350,7 +350,7 @@ def api3(ids,names,passlist):
             fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
             fbbv = str(random.randint(000000000,999999999))
             accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
-            uaddx = f'[lFBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/38.0.0.3137;FBBV/8730088;FBDM/{density=0,width=720,height=1280};FBLC/en_US;FBRV/5477079;FBCR/null;FBMF/Huawei;FBBD/Huawei;FBPN/com.facebook.lite;FBDV/P40 Lite;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
+            uaddx = f'[lFBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/72.0.0.2666;FBBV/5476141;FBDM/{density=,,width=720,height=1280};FBLC/en_US;FBRV/9291926;FBCR/null;FBMF/Huawei;FBBD/Huawei;FBPN/com.facebook.katana;FBDV/Nova Y72;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
             head = {'User-Agent':uaddx,
             'Accept-Encoding':'gzip, deflate',
             'Connection':'close',
