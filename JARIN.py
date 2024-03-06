341ef9ee06db5a40390a5c0503e5a97a1771d8ad
arif-143 <158057368+arif-143@users.noreply.github.com>
2024-03-07 00:24:54 +0600

    Add files via upload

--- JARIN.py
+++ JARIN.py
@@ -268,7 +268,7 @@ def api2(ids,names,passlist):
             fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
             fbbv = str(random.randint(000000000,999999999))
             accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
-            uaddx = f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/51.0.0.9099;FBBV/3426246;FBDM/{density=.,width=720,height=1280};FBLC/en_US;FBRV/8857279;FBCR/null;FBMF/Motorola;FBBD/Motorola;FBPN/com.facebook.adsmanager;FBDV/Edge 20 Lite;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
+            uaddx = f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/39.0.0.7260;FBBV/3380859;FBDM/{density=',width=720,height=1280};FBLC/en_US;FBRV/6724585;FBCR/null;FBMF/Symphony;FBBD/Symphony;FBPN/com.facebook.katana;FBDV/G50;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]'
             head = {'User-Agent':uaddx,
             'Accept-Encoding':'gzip, deflate',
             'Connection':'close',
